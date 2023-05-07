aeeb24355e31fafe49c539168b678cac8286a57e
Allesson Peris pinto de figueiredo <aperis-p@c1r5p1.42sp.org.br>
2023-05-06 22:18:20 -0300

    ft_atoi done

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC = gcc
 FLAGS = -Wall -Werror -Wextra && ./a.out
-SRC = ft_strnstr.c ft_strlen.c
+SRC = ft_atoi.c
 
 all:
 	$(CC) $(SRC) $(FLAGS)
 
 clean:
-	rm -rf ./a.out
+	rm -rf ./a.out    
---
+++ ft_atoi.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 16:56:13 by aperis-p          #+#    #+#             */
+/*   Updated: 2023/05/06 22:17:27 by aperis-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int ft_atoi(const char *nptr)
+{
+    int result;
+    int sign;
+    
+    result = 0;
+    sign = 1;
+    if((*nptr > 32 && *nptr < 43) || (*nptr > 43 && *nptr < 45)
+    || (*nptr > 45 && *nptr < 48) || (*nptr > 57))
+        return (0);
+while(*nptr != '\0')
+{    
+	while((*nptr >= 1 && *nptr <= 32) ||(*nptr == '-' || *nptr == '+'))
+	{
+		if(*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while((*nptr != '\0') && (*nptr >= 48 && *nptr <= 57))
+	{
+		result = (result * 10) + (*nptr - 48); 
+		nptr++;    
+	}
+		return(result * sign);    
+}
+    return(result * sign);
+}
+
+// int main(void)
+// {
+// 	printf("test 0: %d\n", ft_atoi("654")); // = 654
+// 	printf("test 1: %d\n", ft_atoi("sgd")); // = 0
+// 	printf("test 2: %d\n", ft_atoi("s654g654d")); // = 0
+// 	printf("test 3: %d\n", ft_atoi("654g654d")); // = 654
+// 	printf("test 4: %d\n", ft_atoi("9    654")); // = 9
+// 	printf("test 5: %d\n", ft_atoi("    654")); // = 654
+// 	printf("test 6: %d\n", ft_atoi("6.54")); // = 6
+// 	printf("test 7: %d\n", ft_atoi(".654")); // = 0
+// 	printf("test 8: %d\n", ft_atoi("-.654")); // = 0
+// 	printf("test 9: %d\n", ft_atoi("-654")); // = -654
+// 	printf("test 10: %d\n", ft_atoi("	654")); // = 654
+// 	return(0);
+// }
