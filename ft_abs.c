43cd60d960fca63f35625bfcd7de9e31bb5a94b1
Allesson Figueiredo <allfigueiredo@outlook.com>
2023-10-19 16:56:26 -0300

    adding ft_abs | defining INT_MIN, INT_MAX

--- ft_abs.c
+++ ft_abs.c
@@ -1,39 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/29 15:38:27 by aperis-p          #+#    #+#             */
-/*   Updated: 2023/10/15 08:24:31 by aperis-p         ###   ########.fr       */
+/*   Created: 2023/10/14 22:11:48 by aperis-p          #+#    #+#             */
+/*   Updated: 2023/10/17 19:29:15 by aperis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long	ft_atol(const char *str)
+int	ft_abs(int nbr)
 {
-	int		i;
-	int		sign;
-	long	result;
-
-	result = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (sign * result);
+	if (nbr < 0)
+		return (-nbr);
+	else
+		return (nbr);
 }
